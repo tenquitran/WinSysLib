7d9001ecff1eaafe1963dcab6218b27587313d71
tenquitran <tenquitran@gmail.com>
2018-12-17 22:51:57 +0300

    Refactoring

--- WinSysLib/SvcInfo.cpp
+++ WinSysLib/SvcInfo.cpp
@@ -5,9 +5,3 @@
 using namespace WinSysLib;
 
 //////////////////////////////////////////////////////////////////////////
-
-
-SvcInfo::SvcInfo()
-    : m_type(ESvcType::Undefined)
-{
-}
--- WinSysLib/SvcInfo.h
+++ WinSysLib/SvcInfo.h
@@ -6,9 +6,6 @@ namespace WinSysLib
     // Data about the Windows service.
     struct SvcInfo
     {
-    public:
-        SvcInfo();
-        
     private:
         // Service name, or registration name.
         CAtlString m_serviceName;
@@ -17,10 +14,10 @@ namespace WinSysLib
         CAtlString m_displayName;
 
         // Type of the service.
-        ESvcType m_type;
+        ESvcType m_type = { ESvcType::Undefined };
 
         // Current state of the service.
-        ESvcState m_state;
+        ESvcState m_state = { ESvcState::Undefined };
 
         // Load-order group name.
         CAtlString m_groupName;
